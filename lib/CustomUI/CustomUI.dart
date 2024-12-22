d8c253f1a61d9ea64b68f6f5f138fc2ebf576f3d
omar albarakeh <omaralbarakeh2@gmail.com>
2024-12-22 21:17:26 +0200

    flutter <> customui updated

--- lib/CustomUI/CustomUI.dart
+++ lib/CustomUI/CustomUI.dart
@@ -10,6 +10,19 @@ class Customui extends StatefulWidget {
 class _CustomuiState extends State<Customui> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListTile(
+      leading: CircleAvatar(
+        radius: 25,
+      ),
+      title: Text("omar barakeh",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+      subtitle: Row(
+        children: [
+          Icon(Icons.done_all,size: 20,color: Colors.blue,),
+          SizedBox(width: 10,),
+          Text("hey omar",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold)),
+        ],
+      ),
+      trailing: Text("11:11"),
+    );
   }
 }
