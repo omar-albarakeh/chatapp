3ec6f80fccad512f8d2e97bc7042387078787159
omar albarakeh <omaralbarakeh2@gmail.com>
2024-12-22 21:03:17 +0200

    flutter <> customui page add

---
+++ lib/CustomUI/CustomUI.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Customui extends StatefulWidget {
+  const Customui({super.key});
+
+  @override
+  State<Customui> createState() => _CustomuiState();
+}
+
+class _CustomuiState extends State<Customui> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
