6a1687623dc51cbaeb2089e83361f0c47bd5becd
omar albarakeh <omaralbarakeh2@gmail.com>
2024-12-22 22:58:49 +0200

    flutter <> individual page added

---
+++ lib/Pages/individualpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Individualpage extends StatefulWidget {
+  const Individualpage({super.key});
+
+  @override
+  State<Individualpage> createState() => _IndividualpageState();
+}
+
+class _IndividualpageState extends State<Individualpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
