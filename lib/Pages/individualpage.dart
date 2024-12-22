aae98de1280a1a829aba48102f2236a11774726c
omar albarakeh <omaralbarakeh2@gmail.com>
2024-12-22 23:00:17 +0200

    flutter <> individual page updated

--- lib/Pages/individualpage.dart
+++ lib/Pages/individualpage.dart
@@ -10,6 +10,10 @@ class Individualpage extends StatefulWidget {
 class _IndividualpageState extends State<Individualpage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Text("individualpage"),
+      ),
+    );
   }
 }
